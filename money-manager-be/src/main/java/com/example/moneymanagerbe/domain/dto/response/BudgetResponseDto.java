3a63fca28f8ea7a637d68b619d8f64da1ac7512b
phuonghight <hoangphuong270703@gmail.com>
2023-06-20 17:43:56 +0700

    change budget response: extends DateAuditing Dto

--- money-manager-be/src/main/java/com/example/moneymanagerbe/domain/dto/response/BudgetResponseDto.java
+++ money-manager-be/src/main/java/com/example/moneymanagerbe/domain/dto/response/BudgetResponseDto.java
@@ -1,6 +1,5 @@
 package com.example.moneymanagerbe.domain.dto.response;
 
-import com.example.moneymanagerbe.domain.dto.common.DateAuditingDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +9,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
-public class BudgetResponseDto extends DateAuditingDto {
+public class BudgetResponseDto {
 
     private String id;
 
