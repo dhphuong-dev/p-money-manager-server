84787910ef6a371ac3ad7c803150467a5310f072
phuonghight <hoangphuong270703@gmail.com>
2023-07-07 18:51:56 +0700

    [FE] feat: code auth layout

---
+++ money-manager-fe/src/layouts/auth.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import logo from '@/assets/images/auth/logo.png';
+</script>
+
+<template>
+  <div class="container">
+    <div class="auth-head">
+      <div>
+        <img :src="logo" alt="pMoney Manager Logo" />
+      </div>
+      <h2>Best way to saving Your Money!</h2>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use '@styles/variables' as var;
+.auth-head {
+  padding-top: 8rem;
+  padding-bottom: 3.2rem;
+  text-align: center;
+  div {
+    margin-bottom: 3rem;
+  }
+  h2 {
+    font-size: 2rem;
+    line-height: 3.2rem;
+    color: var.$dark;
+  }
+}
+</style>
---
+++ money-manager-fe/src/pages/Home/index.vue
@@ -0,0 +1,11 @@
+<template>Home page</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
+
+<route lang="yaml">
+name: Home
+meta:
+  requiresAuth: true
+</route>
