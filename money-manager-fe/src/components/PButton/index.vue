5cfe3ee233619a595362bea89d56782813a41f1f
phuonghight <hoangphuong270703@gmail.com>
2023-07-08 18:17:46 +0700

    [FE] feat: custom a button for app

---
+++ money-manager-fe/src/components/PButton/index.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { NButton } from 'naive-ui';
+</script>
+
+<template>
+  <n-button
+    :bordered="false"
+    :block="true"
+    icon-placement="left"
+    size="large"
+    color="#0012ff"
+    text-color="white"
+    :builtin-theme-overrides="{
+      textColorHover: '',
+      textColorFocus: '',
+      rippleColor: '',
+      borderRadiusLarge: '5px',
+      fontWeight: 'bold',
+      fontSizeLarge: '5rem',
+      paddingLarge: '2rem 0'
+    }"
+    class="btn"
+  >
+    <slot></slot>
+  </n-button>
+</template>
+
+<style scoped lang="scss">
+.btn {
+  font-size: 3rem !important;
+}
+</style>
