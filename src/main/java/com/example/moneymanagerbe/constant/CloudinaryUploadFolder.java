b9061e1d9d36939e556a282bda3fd4ec4616df2e
phuonghight <hoangphuong270703@gmail.com>
2023-10-07 15:42:25 +0700

    fix: name of upload folder image

--- src/main/java/com/example/moneymanagerbe/constant/CloudinaryUploadFolder.java
+++ src/main/java/com/example/moneymanagerbe/constant/CloudinaryUploadFolder.java
@@ -1,7 +1,7 @@
 package com.example.moneymanagerbe.constant;
 
 public class CloudinaryUploadFolder {
-    public final static String PREFIX = "p-money-manager/";
+    public final static String PREFIX = "p-money-manager";
     public final static String USERS = PREFIX + "/users";
     public final static String TRANSACTIONS = PREFIX + "/transactions";
     public final static String CATEGORIES = PREFIX + "/categories";
